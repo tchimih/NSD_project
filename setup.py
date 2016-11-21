47e6a11423d734431608bd9d7bf1b2b99de73f3d
Anis <anis_hammoutene@hotmail.com>
2016-11-21 21:35:10 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Near Linear Community Detection Algorithm',
+      version='1.0',
+      description='NSD_project',
+      author='Anis Hammoutene',
+      author_email='an.hammoutene@gmail.com',
+      url='https://github.com/tchimih/NSD_project',
+      packages=['networkx', 'matplotlib'],
+     )
